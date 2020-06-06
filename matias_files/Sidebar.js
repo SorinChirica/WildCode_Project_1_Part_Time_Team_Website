a1c08d3ca245d278f67a4d81291ebefbb09ee502
Matias <matias@Gals-MacBook-Pro.local>
2020-06-06 02:52:19 +0200

    responsive navbar added

--- matias_files/Sidebar.js
+++ matias_files/Sidebar.js
@@ -1,26 +1,29 @@
 
+
+
+
+// Sidebar toggle function //
+var toggle = true;
+function toggleSidebar() {
+if (toggle) {
+document.getElementById("mySidebar").style.width = "170px";
+this.toggle = false;
+} else {
+document.getElementById("mySidebar").style.width = "80px";
+document.getElementById("mySidebar").style.opacity = "1";
+this.toggle = true;
+ }
+}
+
+
 document.getElementById("mySidebar").addEventListener("mouseover", mouseOver);
 document.getElementById("mySidebar").addEventListener("mouseout", mouseOut);
+
 function mouseOver() {
   document.getElementById("mySidebar").style.marginRight="0px";
 }
 function mouseOut() {
   document.getElementById("mySidebar").style.marginRight="-50px";
-}
-
-
-
-// Sidebar toggle function //
-var mini = true;
-function toggleSidebar() {
-if (mini) {
-document.getElementById("mySidebar").style.width = "170px";
-this.mini = false;
-} else {
-document.getElementById("mySidebar").style.width = "80px";
-document.getElementById("mySidebar").style.opacity = "1";
-this.mini = true;
- }
 }
 
 // Icon "plus" & "minus" switch function //
